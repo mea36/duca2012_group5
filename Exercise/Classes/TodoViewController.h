472a94f9c9f0df6fbb35fd1231d69b90c259db2b
Seth <furman.seth@gmail.com>
2012-07-24 15:29:28 -0400

    adding workout queue stuff

---
+++ Exercise/Classes/TodoViewController.h
@@ -0,0 +1,24 @@
+//
+//  TodoViewController.h
+//  Exercise
+//
+//  Created by Bennett Furman on 7/24/12.
+//  Copyright (c) 2012 Drexel University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodoViewController : UIViewController {
+    IBOutlet UITextView *todoValue;
+    IBOutlet UITextView *todoSetAmount;    
+    IBOutlet UISegmentedControl *todoUnit;
+    IBOutlet UIButton *todoButton;
+    //Todo *todo;
+}
+
+@property(nonatomic,retain) IBOutlet
+@property(nonatomic,retain) IBOutlet
+@property(nonatomic,retain) IBOutlet
+//@property(nonatomic,retain) Todo *todo;
+
+@end
---
+++ Exercise/Classes/TodoViewController.m
@@ -0,0 +1,44 @@
+//
+//  TodoViewController.m
+//  Exercise
+//
+//  Created by Bennett Furman on 7/24/12.
+//  Copyright (c) 2012 Drexel University. All rights reserved.
+//
+
+#import "TodoViewController.h"
+
+@interface TodoViewController ()
+
+@end
+
+@implementation TodoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
